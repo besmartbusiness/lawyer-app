8e200fe128f23abbc4fd0f50d8be2d2e532b728c
Sebastian Hammer <hammer.seb.ma@gmail.com>
2025-09-21 16:02:30 +0000

    und mir ist noch aufgefallen, dass man in der mobile ansicht manuell etw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { AuthProvider } from '@/lib/hooks/use-auth';
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
